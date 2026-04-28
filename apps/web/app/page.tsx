de82f6a92213a64e105c1e3fd4bef0914cdfe43b
vinod kumar <vinodrao835@gmail.com>
2026-04-28 13:57:58 +0530

    add dev cd workflow

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,5 +1,6 @@
 import prisma from "@repo/db/client";
 
+// this file is staticcally generated at build time
 export default async function Home() {
   console.log("DB URL at next:", process.env.DATABASE_URL);
   const user = await prisma.user.findFirst();
